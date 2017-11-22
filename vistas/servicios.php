6e656a20497427bc8ea7ee9af70d3a1db2e97b5a
esteban1234 <tva_libra@hotmail.com>
2017-11-22 14:59:32 -0600

    inicio de servicios

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -2,117 +2,37 @@
 <?php include 'encabezado2.php' ?>
 <body>
 
-<header >
-    <figure class="logo">
-      <img src="../img/kempo.png" alt="KEMPO" class="img-responsive">
+   <nav class="navbar navbar-inverse" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse"
+            data-target=".navbar-ex1-collapse">
+      <span class="sr-only">ZERBO</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <!-- <a class="navbar-brand" href="#">ZERBO</a> -->
+  </div>
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li><a href="../index.php">INDEX</a></li>
+      <li><a href="nosotros.php">NOSOTROS</a></li>
+      <li class="active"><a href="servicios.php">SERVICIOS</a></li>
+      <li><a href="contacto.php">CONTACTO</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="bannern">
+  <div class="fondon">
+    <figure class="logo pull-left">
+      <img src="../img/zerbo.jpg" alt="ZERBO" class="img-responsive">
     </figure>
-    <nav>
-      <ul>
-        <a class="selector" href="../index.php">INICIO</a>
-        <a class="selector" href="nosotros.php">NOSOTROS</a>
-        <a class="selector" id="select" href="servicios.php">PRODUCTOS</a>
-        <a class="selector" href="contacto.php">CONTACTO</a>
-      </ul>
-    </nav>
-    <div class="op-menu">
-      <i class="fa fa-bars fa-2x abrir" aria-hidden="true"></i>
-    </div>
-  </header>
-
-<div class="container" style="margin-bottom: 3%; ">
-  <h1 class="text-marcas" style="border-top: 1px gray solid; padding-top: 2%;">PRODUCTOS</h1>
-  <hr id="hr">
-  <p class="text-center text-subt" style="margin-top: 3%;">
-    NO BUSQUES MÁS. ELECTRODOMESTICOS Y LÍNEA BLANCA TENEMOS LOS MEJORES PRECIOS <br>
-
-    ¡Porque Somos Distribuidores Mayoristas!</p>
-</div>
-
-<div class="seccion1">
-  <div class="col-md-12 br" style=" padding: 0px; margin:none;">
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen4">
-        
-      </figure>
-      <p class="subtit">Refrigeradores</p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen5">
-        
-      </figure>
-      <p class="subtit">Horno Microondas</p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen6">
-        
-      </figure>
-      <p class="subtit">Horno Tostador</p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen7">
-        
-      </figure>
-      <p class="subtit">Licuadoras</p>
-    </div>
-  </div>
-
-  <div class="col-md-12 br" style=" padding: 0px; margin:none;">
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen8">
-        
-      </figure>
-      <p class="subtit">Estufas</p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen9">
-        
-      </figure>
-      <p class="subtit">Lavadora De Una Tina</p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen10">
-        
-      </figure>
-      <p class="subtit">Secadoras</p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen11">
-        
-      </figure>
-      <p class="subtit">Sandwicheras</p>
-    </div>
-  </div>
-
-  <div class="col-md-12 br" style=" padding: 0px; margin:none;">
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen">
-        
-      </figure>
-      <p class="subtit">Planchas </p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen2">
-        
-      </figure>
-      <p class="subtit">Lavadora De Dos Tinas</p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen3">
-        
-      </figure>
-      <p class="subtit">Aspiradora Koblenz</p>
-    </div>
-    <div class="col-sm-6 col-md-3 unoo">
-      <figure class="imagen12">
-        
-      </figure>
-      <p class="subtit">Batidoras</p>
+    <p class="textbhn pull-right">Somos una empresa sólida dedicada a la venta de textiles.</p>
+    <div class="boton">
+      <a href="contacto.php"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
     </div>
   </div>
 </div>
-<div class="boton2" style="margin-bottom: 5%;">
-    <a href="contacto.php"><button type="button" class="btn btn-warning btn-lg btn-block"><i class="fa fa-phone" aria-hidden="true"></i> CONTACTANOS</button></a>
-  </div>
 
 <a class="go-top" href="#">Subir</a>
 <?php include 'pie.php' ?>
