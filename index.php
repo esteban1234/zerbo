9cf0b167c1f5e73d3707b5407d8cf3f7cc9bc052
esteban1234 <tva_libra@hotmail.com>
2017-11-22 14:58:53 -0600

    index listo

--- index.php
+++ index.php
@@ -2,7 +2,69 @@
 <?php include 'vistas/encabezado.php' ?>
 <body>
 
+<nav class="navbar navbar-inverse" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse"
+            data-target=".navbar-ex1-collapse">
+      <span class="sr-only">ZERBO</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <!-- <a class="navbar-brand" href="#">ZERBO</a> -->
+  </div>
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">INDEX</a></li>
+      <li><a href="vistas/nosotros.php">NOSOTROS</a></li>
+      <li><a href="vistas/servicios.php">SERVICIOS</a></li>
+      <li><a href="vistas/contacto.php">CONTACTO</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="banner">
+  <div class="fondo">
+    <figure class="logo pull-left">
+      <img src="img/zerbo.jpg" alt="ZERBO" class="img-responsive">
+    </figure>
+    <p class="textbh pull-right">Bienvenidos a MANUFACTURERA TEXTILES ZERBO comercializamos los mejores productos textiles hacia todo México y Latino América.</p>
+    <div class="boton">
+      <a href="vistas/contacto.php"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
+    </div>
+  </div>
+</div>
 
+ <div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="img/hilos.jpg"
+            class="img-responsive">
+          </div>
+          <div class="col-md-6">
+            <h1 class="tit">HILOS</h1>
+            <p class="texto">TENEMOS TODOS LOS HILOS DE COSER Y BORDAR, Y LOS TENEMOS JUSTO CUANDO USTED LOS NECESITA.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section" style="padding: 0px 0px 5% 0px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="tit">TELAS</h1>
+            <p class="texto">Telas para la confección de ropa casual, sport, vestidos de fiesta, casimires y todo lo que necesitas de telas para disfraces. Tul, Manta, Pellón, Yute, Satín, terciopelo, organza y chifón en todos los colores y al mejor precio. Sumérgete en nuestra amplia variedad de texturas y estampados.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="img/telas.jpg"
+            class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div clas="boton" style="text-align: center; margin-bottom: 5%;">
+      <a href="vistas/contacto.php"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
+    </div>
 
 
 <a class="go-top" href="#">Subir</a>
--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -1,48 +1,104 @@
 <!DOCTYPE html>
 <?php include 'encabezado2.php' ?>
 <body>
-<header >
-    <figure class="logo">
-      <img src="../img/kempo.png" alt="KEMPO" class="img-responsive">
-    </figure>
-    <nav>
-      <ul>
-        <a class="selector" href="../index.php">INICIO</a>
-        <a class="selector" id="select" href="nosotros.php">NOSOTROS</a>
-        <a class="selector" href="servicios.php">PRODUCTOS</a>
-        <a class="selector" href="contacto.php">CONTACTO</a>
-      </ul>
-    </nav>
-    <div class="op-menu">
-      <i class="fa fa-bars fa-2x abrir" aria-hidden="true"></i>
-    </div>
-  </header>
-
-<div class="container" style="margin-bottom: 3%; ">
-  <h1 class="text-marcas" style="border-top: 1px gray solid; padding-top: 2%;">¿Quiénes Somos?</h1>
-  <hr id="hr">
-</div>
-
-<div class="container">
-  <h1 class="mvv">Misión</h1>
-  <p class="text-mision">Ser la mejor empresa comercializadora de aparatos electrodomésticos y artículos para el hogar (linea blanca) que satisfaga las necesidades y expectativas de los clientes en el mercado nacional y de exportación. Con un equipo humano capaz, innovador y realizado, logrando una rentabilidad en un ambiente de competencia internacional que asegure la permanencia y el crecimiento de la empresa, garantice una retribución adecuada a nuestros colaboradores y accionistas, cumpliendo con la responsabilidad social y ecológica que nos corresponde.
-</p>
-
-<h1 class="mvv">Visión</h1>
-  <p class="text-mision">Desarrollar altos niveles en servicio, calidad y con tecnología de punta, asociada a la responsabilidad y el compromiso, que nos permita permanecer como la principal opción de compra para nuestros clientes, así como también nos ubicara como el primer distribuidor de electrodomésticos y aparatos de línea blanca mayorista a nivel nacional para finalmente captar nuevos clientes y abarcar mas zonas en todo el país.
-</p>
-
-<h1 class="mvv">Valores</h1>
-  <p class="text-mision">Confiabilidad: valoramos la honestidad, la integridad, la lealtad y nuestra capacidad de cumplir lo prometido. <br><br>
-
-Humanismo: estamos comprometidos con una conducta ética y solidaria con el desarrollo de todos los sectores de la sociedad.<br><br>
-
-Responsabilidad: nuestras acciones repercuten en nuestro entorno, somos responsables de lo que hacemos y la responsabilidad exige de nosotros la excelencia.
-</p>
-</div>
-<div class="boton2" style="margin-bottom: 5%;">
-    <a href="contacto.php"><button type="button" class="btn btn-warning btn-lg btn-block"><i class="fa fa-phone" aria-hidden="true"></i> CONTACTANOS</button></a>
+    
+    <nav class="navbar navbar-inverse" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse"
+            data-target=".navbar-ex1-collapse">
+      <span class="sr-only">ZERBO</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <!-- <a class="navbar-brand" href="#">ZERBO</a> -->
   </div>
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li><a href="../index.php">INDEX</a></li>
+      <li class="active"><a href="nosotros.php">NOSOTROS</a></li>
+      <li><a href="servicios.php">SERVICIOS</a></li>
+      <li><a href="contacto.php">CONTACTO</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="bannern">
+  <div class="fondon">
+    <figure class="logo pull-left">
+      <img src="../img/zerbo.jpg" alt="ZERBO" class="img-responsive">
+    </figure>
+    <p class="textbhn pull-right">Somos una empresa sólida dedicada a la venta de textiles.</p>
+    <div class="boton">
+      <a href="contacto.php"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
+    </div>
+  </div>
+</div>
+
+<div class="section" style="padding: 5% 0px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="../img/nosotros-1.jpg"
+            class="img-responsive">
+          </div>
+          <div class="col-md-6">
+            <h1 style="color: ">¿QUIENES SOMOS?</h1>
+            <h3>MANUFACTURERA TEXTILES ZERBO</h3>
+            <p class="texto2">La mejor opcion en Mexico, en logistica y comercializacion, para importar, distribuir, comercializar, representar y administrar su empresa y sus mercancias del ramo textil, con una experiencia en el mercado internacional y Mexicano. Cubriendo principalmente el segmento de "TIENDAS DE TEXTILES". <br><br>
+            Contamos con colaboradores y muchas historias de éxito en las cuales el factor principal has sido tú.
+
+            Queremos ser una empresa institucional y moderna, lo cual significa que sea ordenada, disciplinada, con procesos y resultados firmes.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<div class="section" style="padding: 5% 0px; background-color: #eeeeee;">
+      <div class="container">
+        <div class="row">
+          <div class="uno">
+            <div class="icono">
+              <i class="fa fa-truck fa-5x" aria-hidden="true"></i>
+            </div>
+            <h1 class="text-center">MISION</h1>
+            <p class="textom">Comercializar productos de gran calidad a bajo costo, para confeccionar prendas y decorar su hogar.</p>
+          </div>
+
+          <div class="dos">
+            <div class="icono">
+              <i class="fa fa-line-chart fa-5x" aria-hidden="true"></i>
+            </div>
+            <h1 class="text-center">VISION</h1>
+            <p class="textom">Ser la empresa líder en el ramo textil, ofreciendo los precios más bajos garantizando la calidad y servicio.</p>
+          </div>
+
+          <div class="tres">
+            <div class="icono">
+              <i class="fa fa-heartbeat fa-5x" aria-hidden="true"></i>
+            </div>
+            <h1 class="text-center">VALORES</h1>
+            <p class="textom">Honestidad, Lealtad, Compromiso, Congruencia, Etica, Creatividad, Agilidad y Humildad.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section" style="padding: 5% 0px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            
+            <p class="texto1">En MANUFACTURERA TEXTILES ZERBO encontrarás la mayor variedad en telas para confeccionar la ropa de toda la familia y la decoración del hogar. Contamos con productos que te ayudarán a concretar tus ideas como telas para vestidos de noche, faldas, blusas, pantalones, trajes, sudaderas, playeras, camisas, cortinas, ropa de cama, manteles, sábanas, cojines, trapos de cocina, jergas, franelas, tapicería, etc. <br><br>
+
+            Así mismo, ponemos a tu disposición nuestro amplio surtido en mercería como hilos, listones, cintas, cierres, así como toda la gama de productos para trabajos con manualidades.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="../img/telas2.jpg"
+            class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 <a class="go-top" href="#">Subir</a>
 <?php include 'pie.php' ?>
